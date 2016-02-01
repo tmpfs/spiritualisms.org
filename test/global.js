d6c1fa38a554f0fb4d11ed89f62ded2c64d4b802
muji <freeformsystems@gmail.com>
2016-02-01 20:50:11 +0800

    Start api server in test run.

--- test/global.js
+++ test/global.js
@@ -1,7 +1,10 @@
-var www = require('../www/server');
-process.env.WWW = process.env.WWW || 'http://localhost:3000';
+var www = require('../www/server')
+  , api = require('../api/server');
+process.env.WWW = process.env.WWW || 'http://localhost:4000';
+process.env.API = process.env.API || 'http://localhost:4001';
 
 before(function(done) {
-  www.listen(3000);
+  www.listen(4000);
+  api.listen(4001);
   done();
 })
