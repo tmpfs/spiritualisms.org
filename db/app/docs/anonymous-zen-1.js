1661334bf7ea81fc93246f835b9003368c81dce3
muji <freeformsystems@gmail.com>
2016-02-01 23:57:27 +0800

    Update contributing and add quote.
    Tidy inspire output to handle quotes with no link.

---
+++ db/app/docs/anonymous-zen-1.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'quote',
+  quote: 'The plants, they don\'t scream as loud.',
+  author: 'Anonymous Zen Master',
+  link: '',
+  domain: '',
+  created: new Date().getTime(),
+  random: Math.random(),
+  home: true,
+  tags: [
+    'zen'
+  ]
+}
