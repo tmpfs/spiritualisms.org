f05c2c310cefd24f883c4b7f561190e8b7ad07f2
muji <freeformsystems@gmail.com>
2016-02-01 23:23:40 +0800

    Improve create page.

---
+++ db/app/docs/tagore-1.js
@@ -0,0 +1,15 @@
+module.exports = {
+  type: 'quote',
+  quote: 'Love is the ultimate truth at the heart of creation.',
+  author: 'Rabindranath Tagore',
+  link: 'https://en.wikipedia.org/wiki/Rabindranath_Tagore',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  home: true,
+  tags: [
+    'wisdom',
+    'love',
+    'creation'
+  ]
+}
