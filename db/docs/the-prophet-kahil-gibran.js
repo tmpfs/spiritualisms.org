1f253da6a9a6423d83791f12f40ccf644dfaa49f
muji <freeformsystems@gmail.com>
2016-02-01 18:26:43 +0800

    Add draft db docs.

---
+++ db/docs/the-prophet-kahil-gibran.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'quote',
+  quote: 'You give but little when you give of your possessions. It is when you give of yourself that you truly give.',
+  author: 'Kahil Gibran',
+  link: 'https://en.wikipedia.org/wiki/Khalil_Gibran',
+  isbn: {
+    10: '0439451507',
+    13: ['9780439451505', '9780681372054']
+  },
+  tags: [
+    'giving'
+  ]
+}
