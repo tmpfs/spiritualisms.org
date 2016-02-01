6d4a24d2ca9288629a1d6dc4a9def6f356699585
muji <freeformsystems@gmail.com>
2016-02-01 20:34:52 +0800

    Assert on quotes model.

--- test/spec/model/quote.js
+++ test/spec/model/quote.js
@@ -5,8 +5,16 @@ describe('model:', function() {
 
   it('should list quotes', function(done) {
     var quote = new Quote();
-    quote.list({}, function(err, res, body) {
-      console.dir(err); 
+    quote.list(null, function(err, res, body) {
+      expect(err).to.eql(null);
+      expect(res).to.be.an('object');
+      expect(body).to.be.an('object');
+      expect(body.rows).to.be.an('array')
+        .to.have.length.gt(0);
+      var quote = body.rows[0];
+      expect(quote).to.be.an('object');
+      expect(quote.id).to.be.a('string');
+      expect(quote.doc).to.be.an('object');
       done();
     })
   })
