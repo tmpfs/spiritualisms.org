8fe04588c937dbbd0ecde518ed4e7c4f1c6a2fa5
muji <freeformsystems@gmail.com>
2016-02-02 10:32:30 +0800

    Add quotation by Gautama Buddha on happiness.

---
+++ db/app/docs/buddha-1.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'quote',
+  publish: true,
+  quote: 'Happiness does not depend on what you have or who you are, it solely relies on what you think.',
+  author: 'Gautama Buddha',
+  link: 'https://en.wikipedia.org/wiki/Gautama_Buddha',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'happiness'
+  ]
+}
