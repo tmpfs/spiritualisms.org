0c2fee8b8e5c58a78a9e53542618aee57d220410
muji <freeformsystems@gmail.com>
2016-02-02 09:56:55 +0800

    Add quote/random endpoint and model logic.

--- api/server.js
+++ api/server.js
@@ -20,6 +20,18 @@ app.get('/quote/list', function(req, res, next) {
   })
 });
 
+app.get('/quote/random', function(req, res, next) {
+  var quote = new Quote()
+    , opts = {};
+  quote.random(opts, function(err, response, body) {
+    if(err) {
+      return next(err);
+    }
+    res.set('content-type', 'application/json');
+    res.send(JSON.stringify(body));
+  })
+});
+
 app.get('/quote/:id', function(req, res, next) {
   var quote = new Quote()
     , opts = {id: req.params.id};
--- db/app/views/random.js
+++ db/app/views/random.js
@@ -1,8 +1,10 @@
+/* jshint ignore:start */
 module.exports = {
   map: function(doc) {
-    if(doc.type === 'quote') {
+    if(doc.type === 'quote' && doc.publish === true) {
       emit(doc.random, null);
     }
   },
   reduce: '_count'
 }
+/* jshint ignore:end */
