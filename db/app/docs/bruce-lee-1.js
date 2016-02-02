289f0d3a3d3b2d2c659a01f3513b10dcea0599de
muji <freeformsystems@gmail.com>
2016-02-02 12:18:52 +0800

    Add bruce lee quote.
    Fix space issue with title attribute.

---
+++ db/app/docs/bruce-lee-1.js
@@ -0,0 +1,15 @@
+module.exports = {
+  type: 'quote',
+  publish: true,
+  quote: 'Empty your mind, be formless. Shapeless, like water. [...] Be water, my friend.',
+  author: 'Bruce Lee',
+  link: 'https://www.youtube.com/watch?v=APx2yFA0-B4',
+  domain: 'youtube.com',
+  created: new Date().getTime(),
+  random: Math.random(),
+  home: true,
+  tags: [
+    'life',
+    'inspirational'
+  ]
+}
