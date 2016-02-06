0500c97aafc8edd1ba82c9a755f2691543403ce9
muji <freeformsystems@gmail.com>
2016-02-06 11:20:22 +0800

    Add quote.

---
+++ db/app/docs/iyengar-1.js
@@ -0,0 +1,20 @@
+module.exports = {
+  _id: 'food-is-brahman',
+  type: 'quote',
+  publish: true,
+  quote: 'Food is Brahman.',
+  author: 'B.K.S. Iyengar',
+  link: 'http://www.bksiyengar.com',
+  domain: 'bksiyengar.com',
+  citation: {
+    type: 'book',
+    title: 'Light on Pranayama',
+    page: 52,
+    isbn: '978-81-7223-541-3'
+  },
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'food'
+  ]
+}
