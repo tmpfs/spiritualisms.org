086bf80135db6bb081a837a4f63224e91df78c90
muji <freeformsystems@gmail.com>
2016-02-08 08:22:12 +0800

    Add quote, update dep.

---
+++ db/app/docs/patanjali-1.js
@@ -0,0 +1,15 @@
+module.exports = {
+  _id: 'progress-in-meditation',
+  type: 'quote',
+  publish: true,
+  quote: 'Progress in meditation comes swiftly for those who try their hardest.',
+  author: 'Patanjali',
+  link: 'https://en.wikipedia.org/wiki/Patanjali',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'progress',
+    'meditation'
+  ]
+}
