22249649d83e3400c2cc1feeea4080986ce1a618
muji <freeformsystems@gmail.com>
2016-02-11 19:39:15 +0800

    Add quotes.

---
+++ db/app/docs/max-ehrmann-1.js
@@ -0,0 +1,20 @@
+module.exports = {
+  _id: 'go-placidly-amid',
+  type: 'quote',
+  publish: true,
+  quote: 'Go placidly amid the noise and the haste, and remember what peace there may be in silence.',
+  author: 'Max Ehrmann',
+  link: 'https://en.wikipedia.org/wiki/Desiderata',
+  domain: 'en.wikipedia.org',
+  citation: {
+    type: 'poem',
+    title: 'Desiderata',
+    isbn: '9780517534229'
+  },
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'peace',
+    'silence'
+  ]
+}
