b72c96636793beba9864672c933ac6a7777a5556
muji <freeformsystems@gmail.com>
2016-11-22 11:36:32 +0800

    Add quotation.

---
+++ db/app/docs/tao-huang-mantak-chia-1.js
@@ -0,0 +1,20 @@
+module.exports = {
+  _id: 'contentment-arises-from-non-attachment',
+  type: 'quote',
+  publish: true,
+  quote: 'Contentment arises from non-attachment.',
+  author: ['Mantak Chia', 'Tao Huang'],
+  link: 'http://www.universal-tao.com',
+  domain: 'universal-tao.com',
+  citation: {
+    type: 'book',
+    title: 'The Secret Teachings of the Tao Te Ching',
+    page: 0,
+    isbn: '978-089281191-5'
+  },
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'tao'
+  ]
+}
