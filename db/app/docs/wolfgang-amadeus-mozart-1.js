2889b76ba2b642d4999895f5f3b6ee90125082da
muji <freeformsystems@gmail.com>
2016-11-22 12:15:24 +0800

    Add quotation.

---
+++ db/app/docs/wolfgang-amadeus-mozart-1.js
@@ -0,0 +1,15 @@
+module.exports = {
+  _id: 'silence-between-them',
+  type: 'quote',
+  publish: true,
+  quote: 'The music is not in the notes but in the silence between them.',
+  author: 'Wolfgang Amadeus Mozart',
+  link: 'https://en.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'music',
+    'silence'
+  ]
+}
