4235dff81ca613e6d3babc651ee37fc905e18115
muji <freeformsystems@gmail.com>
2016-11-25 11:22:24 +0800

    Add quotation.

---
+++ db/app/docs/leo-tolstoy-1.js
@@ -0,0 +1,14 @@
+module.exports = {
+  _id: 'happiness-does-not-depend',
+  type: 'quote',
+  publish: true,
+  quote: 'Happiness does not depend on outward things, but on the way we see them.',
+  author: 'Leo Tolstoy',
+  link: 'https://en.wikipedia.org/wiki/Leo_Tolstoy',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'happiness'
+  ]
+}
