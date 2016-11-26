376feaba27a2d8f9ac962f3f5bd25bd9e7213350
muji <freeformsystems@gmail.com>
2016-11-26 13:56:59 +0800

    Add quotation.

---
+++ db/app/docs/dag-hammarskjold-1.js
@@ -0,0 +1,15 @@
+module.exports = {
+  _id: 'our-ideas-about-death',
+  type: 'quote',
+  publish: true,
+  quote: 'Our ideas about death define how we live our lives.',
+  author: 'Dag Hammarskjöld',
+  link: 'https://en.wikipedia.org/wiki/Dag_Hammarskj%C3%B6ld',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'life',
+    'death'
+  ]
+}
