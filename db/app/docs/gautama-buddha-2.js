ff2fb345962c411cd5979ac7abf07182ed2479b3
muji <freeformsystems@gmail.com>
2016-11-28 11:16:21 +0800

    Add quotation, move quotation.

---
+++ db/app/docs/gautama-buddha-2.js
@@ -0,0 +1,14 @@
+module.exports = {
+  _id: '',
+  type: 'quote',
+  publish: true,
+  quote: 'The mind is everything. What you think you become.',
+  author: 'Gautama Buddha',
+  link: 'https://en.wikipedia.org/wiki/Gautama_Buddha',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'mind'
+  ]
+}
