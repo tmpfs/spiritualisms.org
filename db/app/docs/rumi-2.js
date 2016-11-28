9dedc93e6cd6a5516f955c128b067f4368991cde
muji <freeformsystems@gmail.com>
2016-11-28 12:05:51 +0800

    Add quotation.

---
+++ db/app/docs/rumi-2.js
@@ -0,0 +1,14 @@
+module.exports = {
+  _id: 'there-is-a-voice',
+  type: 'quote',
+  publish: true,
+  quote: 'There is a voice that doesn’t use words. Listen.',
+  author: 'Rumi',
+  link: 'https://en.wikipedia.org/wiki/Rumi',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'listen'
+  ]
+}
