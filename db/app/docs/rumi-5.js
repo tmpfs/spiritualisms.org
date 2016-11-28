b8059ee15fd2313e7e26aee695a927f714ed50b6
muji <freeformsystems@gmail.com>
2016-11-28 12:58:50 +0800

    Add quotation.

---
+++ db/app/docs/rumi-5.js
@@ -0,0 +1,14 @@
+module.exports = {
+  _id: 'there-are-a-thousand-ways',
+  type: 'quote',
+  publish: true,
+  quote: 'There are a thousand ways to kneel and kiss the earth.',
+  author: 'Rumi',
+  link: 'https://en.wikipedia.org/wiki/Rumi',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'kiss'
+  ]
+}
