65581dd429a4109494613ac2aa6aa3f4aaaf0282
muji <freeformsystems@gmail.com>
2016-11-28 18:57:46 +0800

    Add quotations.

---
+++ db/app/docs/rumi-11.js
@@ -0,0 +1,15 @@
+module.exports = {
+  _id: 'raise-your-words',
+  type: 'quote',
+  publish: true,
+  quote: 'Raise your words, not voice. It is rain that grows flowers, not thunder.',
+  author: 'Rumi',
+  link: 'https://en.wikipedia.org/wiki/Rumi',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'rain',
+    'flowers'
+  ]
+}
---
+++ db/app/docs/rumi-9.js
@@ -0,0 +1,14 @@
+module.exports = {
+  _id: 'sell-your-cleverness',
+  type: 'quote',
+  publish: true,
+  quote: 'Sell your cleverness and buy bewilderment.',
+  author: 'Rumi',
+  link: 'https://en.wikipedia.org/wiki/Rumi',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'bewilderment'
+  ]
+}
