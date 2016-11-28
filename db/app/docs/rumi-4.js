813254a1c71033fc2728cec19f76f9270d87090d
muji <freeformsystems@gmail.com>
2016-11-28 12:56:49 +0800

    Add quotation.

---
+++ db/app/docs/rumi-4.js
@@ -0,0 +1,14 @@
+module.exports = {
+  _id: 'beyond-right-and-wrong',
+  type: 'quote',
+  publish: true,
+  quote: 'Beyond right and wrong, there is a field. I will meet you there.',
+  author: 'Rumi',
+  link: 'https://en.wikipedia.org/wiki/Rumi',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'ethics'
+  ]
+}
