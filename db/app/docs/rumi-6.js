f2a9b45d3feeb6254a9d3619a6081bc1dba3b7c5
muji <freeformsystems@gmail.com>
2016-11-28 18:18:04 +0800

    Add quotations.

---
+++ db/app/docs/rumi-6.js
@@ -0,0 +1,16 @@
+module.exports = {
+  _id: 'when-you-do-things-from-your-soul',
+  type: 'quote',
+  publish: true,
+  quote: 'When you do things from your soul, you feel a river moving in you, a joy.',
+  author: 'Rumi',
+  link: 'https://en.wikipedia.org/wiki/Rumi',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'soul',
+    'river',
+    'joy'
+  ]
+}
