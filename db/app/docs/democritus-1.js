2c3a7cacbbcb951f0a021301f5bab39a8e4cf0e4
muji <freeformsystems@gmail.com>
2016-11-28 11:06:01 +0800

    Add quotation.

---
+++ db/app/docs/democritus-1.js
@@ -0,0 +1,14 @@
+module.exports = {
+  _id: 'to-a-wise-man',
+  type: 'quote',
+  publish: true,
+  quote: 'To a wise man, the whole earth is open, because the true country of a virtuous soul is the entire universe.',
+  author: 'Democritus',
+  link: 'https://en.wikipedia.org/wiki/Democritus',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'wisdom'
+  ]
+}
