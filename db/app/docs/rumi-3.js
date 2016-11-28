6a99f61c46015507c44226e83a513011ca1046e4
muji <freeformsystems@gmail.com>
2016-11-28 12:07:33 +0800

    Add quotation.

---
+++ db/app/docs/rumi-3.js
@@ -0,0 +1,14 @@
+module.exports = {
+  _id: 'what-you-seek',
+  type: 'quote',
+  publish: true,
+  quote: 'What you seek is seeking you.',
+  author: 'Rumi',
+  link: 'https://en.wikipedia.org/wiki/Rumi',
+  domain: 'en.wikipedia.org',
+  created: new Date().getTime(),
+  random: Math.random(),
+  tags: [
+    'seek'
+  ]
+}
