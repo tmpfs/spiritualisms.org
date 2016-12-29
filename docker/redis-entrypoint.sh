9b38e0581dfe6570d5c55cb0d90a25d23fffba1a
muji <freeformsystems@gmail.com>
2016-12-29 11:47:14 +0800

    Fix wkhtmltoimage path bug.
    Move sync program.

--- docker/redis-entrypoint.sh
+++ docker/redis-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sysctl -w net.core.somaxconn=65535
+sysctl -w net.core.somaxconn=65535 || true
 
 redis-server /opt/redis/redis.conf
 
--- file-server.js
+++ file-server.js
@@ -1,4 +1,10 @@
 process.env.FILES = process.env.FILES || '//file.spiritualisms.org';
 
+// shouldn't need nginx proxy in devel
+var env = require('nenv')();
+if(env.devel) {
+  process.env.FILES = 'http://localhost:3002'; 
+}
+
 var app = require('./file/server');
 app.listen(3002);
