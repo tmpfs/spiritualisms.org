2252259e1ff0bdfb1e28427b26a988e662ddbcc8
muji <freeformsystems@gmail.com>
2016-12-29 12:04:49 +0800

    Update domains used in devel environment.

--- api-server.js
+++ api-server.js
@@ -1,2 +1,10 @@
+process.env.API = process.env.API || '//api.spiritualisms.org';
+
+// shouldn't need nginx proxy in devel
+var env = require('nenv')();
+if(env.devel) {
+  process.env.API = 'http://localhost:3001';
+}
+
 var app = require('./api/server');
 app.listen(3001);
